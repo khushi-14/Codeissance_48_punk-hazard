ba0c319fa0820ca31ce32a261bb1399c090de30e
Sourav Macwan <souravawem@gmail.com>
2022-09-23 18:36:17 +0530

    payment gateway

---
+++ Help-Pact/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, redirect, request, render_template
+
+import stripe
+# This is your test secret API key.
+stripe.api_key = 'sk_test_51Ll94ISAAg18wyqJX2nsGW6qrr5Lw7LooFrfLhJNZEdVwQ9htxTSE39JCyF7xPgH8ySL07BFXuz6ybOM9WbvpSMK005shgd3oM'
+
+app = Flask(__name__)
+
+YOUR_DOMAIN = 'http://localhost:5500'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('checkout.html')
+@app.route('/create-checkout-session', methods=['POST'])
+def create_checkout_session():
+    try:
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                    'price': 'price_1Ll96sSAAg18wyqJWbNTBqBf',
+                    'quantity': 1,
+                },
+            ],
+            mode='subscription',
+            success_url=YOUR_DOMAIN + '/success.html',
+            cancel_url=YOUR_DOMAIN + '/cancel.html',
+        )
+    except Exception as e:
+        return str(e)
+
+    return redirect(checkout_session.url, code=303)
+
+
+if __name__ == '__main__':
+    app.run(port=5500,debug=True)
