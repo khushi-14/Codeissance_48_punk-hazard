17ac644693fa2c305de1579efa68483b8480dacc
Girish Rajani <girish.r.rajani@gmail.com>
2022-09-24 10:33:04 +0530

    Twilio Merge

--- Help-Pact/server.py
+++ Help-Pact/server.py
@@ -1,7 +1,20 @@
 from flask import Flask, render_template, redirect
 import stripe
+
+from notify_run import Notify
 # This is your test secret API key.
 stripe.api_key = 'sk_test_51Ll94ISAAg18wyqJX2nsGW6qrr5Lw7LooFrfLhJNZEdVwQ9htxTSE39JCyF7xPgH8ySL07BFXuz6ybOM9WbvpSMK005shgd3oM'
+
+import os
+from twilio.rest import Client
+
+notify = Notify()
+notify.register()
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = ""
+auth_token = ""
+client = Client(account_sid, auth_token)
 
 app = Flask(__name__)
 
@@ -26,6 +39,18 @@ def portal():
 @app.route('/register')
 def register():
     return render_template('home/registration.html')
+
+@app.route('/call')
+def call():
+    # call = client.calls.create(
+    #                     twiml= '<Response><Say>Car Accident at Bandra</Say></Response>',
+    #                     to='+918655708348',
+    #                     from_='+19706485637'
+                        
+    #                 )
+    # print(call.sid)
+    notify.send("Any Message you want to send")
+    return redirect('/')
 
 @app.route('/socialworker')
 def socialworker():
